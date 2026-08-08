42c27b3e74f401aaafdc060ac9d10d3e6056ae55
Fernando Tapia <ferxdtg@gmail.com>
2026-08-08 18:06:48 -0500

    modificaciones080826

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,22 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Features from "../components/Features";
+import Testimonials from "../components/Testimonials"; // 1. Importa el componente
 import Categories from "../components/Categories";
 import Products from "../components/Products";
 import Footer from "../components/Footer";
 
-
 export default function Home(){
-
-
-return (
-
-<>
-
-
-<Navbar />
-
-<Hero />
-
-<Features />
-
-<Categories />
-
-<Products />
-
-<Footer />
-
-
-</>
-
-
-);
-
-
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      
+      <Categories />
+      <Testimonials /> {/* 2. Colócalo aquí para que se muestre */}
+      <Products />
+      <Footer />
+    </>
+  );
 }
